028fad2728825388bc1b4c97e749198686216495
codedjay672 <emmajoel6456@gmail.com>
2025-02-19 18:08:27 +0100

    add production url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,6 +2,7 @@ const config = {
   env: {
     endpoint: process.env.NEXT_PUBLIC_ENDPOINT_URL!,
     databaseUrl: process.env.DATABASE_URL!,
+    prodEndpoint: process.env.NEXT_PUBLIC_PROD_ENDPOINT!,
     imagekit: {
       urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_API_ENDPOINT!,
       publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
